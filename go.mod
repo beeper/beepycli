056574303c8b920d9620ec85e2c8d85f108a49c9
FIGBERT <figbert@figbert.com>
2023-07-31 18:30:22 -0700

    Add SSH stage

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/figbert/beepy
 go 1.20
 
 require (
+	github.com/charmbracelet/bubbles v0.16.1
 	github.com/charmbracelet/bubbletea v0.24.2
 	github.com/charmbracelet/lipgloss v0.7.1
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- main.go
+++ main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"strings"
 
-	//"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbletea"
 	gloss "github.com/charmbracelet/lipgloss"
+
+	"github.com/charmbracelet/bubbles/textinput"
 )
 
 type phase int
 
 const (
 	welcome phase = iota
-	username
-	host
+	ssh
+	next
 )
 
 type model struct {
 	phase          phase
 	focusOnButton  bool
-	username, host string
+	username, host textinput.Model
 }
 
 func (m model) Init() tea.Cmd {
@@ -32,10 +33,37 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		case tea.KeyEnter, tea.KeyTab:
-			if !m.focusOnButton {
-				m.focusOnButton = true
-				return m, nil
+			switch m.phase {
+			case welcome:
+				if !m.focusOnButton {
+					m.focusOnButton = true
+				} else {
+					m.phase = ssh
+					m.focusOnButton = false
+				}
+			case ssh:
+				if m.host.Focused() {
+					m.focusOnButton = true
+					m.host.Blur()
+				} else if m.username.Focused() {
+					m.username.Blur()
+					m.username.CursorStart()
+					m.host.Focus()
+				} else if m.focusOnButton {
+					m.phase = next
+					m.focusOnButton = false
+				} else {
+					m.username.Focus()
+				}
 			}
+		default:
+			var cmd tea.Cmd
+			if m.username.Focused() {
+				m.username, cmd = m.username.Update(msg)
+			} else if m.host.Focused() {
+				m.host, cmd = m.host.Update(msg)
+			}
+			return m, cmd
 		}
 	}
 	return m, nil
@@ -44,18 +72,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	var builder strings.Builder
 
+	title := gloss.NewStyle().Bold(true)
+	button := gloss.NewStyle().
+		Background(gloss.Color("8")).
+		Padding(0, 2)
+	if m.focusOnButton {
+		button = button.Background(gloss.Color("13"))
+	}
+
 	switch m.phase {
 	case welcome:
-		background := gloss.Color("8")
-		if m.focusOnButton {
-			background = gloss.Color("13")
-		}
-
-		title := gloss.NewStyle().Bold(true)
-		button := gloss.NewStyle().
-			Background(background).
-			Padding(0, 2)
-
 		builder.WriteString(title.Render("Hello! Welcome to the Beepy Setup Wizard™"))
 		builder.WriteString("\nA quick guide to navigating the Wizard:\n\n")
 		builder.WriteString("\t↹ Tab|⏎ Return\n")
@@ -66,6 +92,12 @@ func (m model) View() string {
 		builder.WriteString("\t\tQuit\n\n")
 		builder.WriteString("We hope you enjoy your time with the Wizard 🧙!\n\n")
 		builder.WriteString(button.Render("Next"))
+	case ssh:
+		builder.WriteString(title.Render("Configure network access to your Beepy"))
+		builder.WriteString("\nThe Wizard accesses your device over SSH.\n\n")
+		builder.WriteString("\tssh " + m.username.View() + "@" + m.host.View() + "\n\n")
+		builder.WriteString("This is extremely required.\n\n")
+		builder.WriteString(button.Render("Next"))
 	default:
 		builder.WriteString("How did we get here?")
 	}
@@ -75,7 +107,17 @@ func (m model) View() string {
 
 func main() {
 	m := model{
-		phase: welcome,
+		phase:    welcome,
+		username: textinput.New(),
+		host:     textinput.New(),
 	}
+
+	m.username.Placeholder = "eric"
+	m.username.Prompt = ""
+	m.username.Cursor.Style = gloss.NewStyle().Foreground(gloss.Color("13"))
+	m.host.Placeholder = "192.0.0.1"
+	m.host.Prompt = ""
+	m.username.Cursor.Style = gloss.NewStyle().Foreground(gloss.Color("13"))
+
 	tea.NewProgram(m).Run()
 }
