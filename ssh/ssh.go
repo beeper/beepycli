25dc5dd585c723fc865a2202dc15f3f653234797
FIGBERT <figbert@figbert.com>
2023-08-11 00:08:31 -0700

    Add password and validation to ssh stage

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -10,14 +10,16 @@ import (
 )
 
 type Model struct {
-	username, host textinput.Model
-	buttonFocused  bool
+	username, host, password, confirmation textinput.Model
+	buttonFocused, valid                   bool
 }
 
 func InitModel() Model {
 	return Model{
-		username: utils.TextInput("user", false),
-		host:     utils.TextInput("127.0.0.1", false),
+		username:     utils.TextInput("beepy", false),
+		host:         utils.TextInput("127.0.0.1", false),
+		password:     utils.TextInput("beepbeep", true),
+		confirmation: utils.TextInput("beepbeep", true),
 	}
 }
 
@@ -32,10 +34,18 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		case tea.KeyEnter, tea.KeyTab:
 			if m.buttonFocused {
-				return m, utils.NextPhase
-			} else if m.host.Focused() {
+				if len(m.username.Value()) > 0 && len(m.host.Value()) > 0 && m.valid {
+					return m, utils.NextPhase
+				}
+			} else if m.confirmation.Focused() {
+				m.confirmation.Blur()
 				m.buttonFocused = true
+			} else if m.password.Focused() {
+				m.password.Blur()
+				m.confirmation.Focus()
+			} else if m.host.Focused() {
 				m.host.Blur()
+				m.password.Focus()
 			} else if m.username.Focused() {
 				m.username.Blur()
 				m.username.CursorStart()
@@ -47,6 +57,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyShiftTab:
 			if m.buttonFocused {
 				m.buttonFocused = false
+				m.confirmation.Focus()
+			} else if m.confirmation.Focused() {
+				m.confirmation.Blur()
+				m.password.Focus()
+			} else if m.password.Focused() {
+				m.password.Blur()
 				m.host.Focus()
 			} else if m.host.Focused() {
 				m.username.CursorEnd()
@@ -64,23 +80,43 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.username, cmd = m.username.Update(msg)
 			} else if m.host.Focused() {
 				m.host, cmd = m.host.Update(msg)
+			} else if m.password.Focused() {
+				m.password, cmd = m.password.Update(msg)
+				cmd = tea.Batch(cmd, validatePassword(m.password.Value(), m.confirmation.Value()))
+			} else if m.confirmation.Focused() {
+				m.confirmation, cmd = m.confirmation.Update(msg)
+				cmd = tea.Batch(cmd, validatePassword(m.password.Value(), m.confirmation.Value()))
 			}
 			return m, cmd
 		}
+	} else if validation, ok := msg.(passwordValidationMsg); ok {
+		m.valid = bool(validation)
 	}
 	return m, nil
 }
 
 func (m Model) View() string {
+	validation := ""
+	if len(m.confirmation.Value()) > 0 {
+		if m.valid {
+			validation = fmt.Sprintf(" %s", okValidation.Render())
+		} else {
+			validation = fmt.Sprintf(" %s", errValidation.Render())
+		}
+	}
+
 	return fmt.Sprintf(
 		"%s\n"+
 			"The Wizard accesses your device over SSH.\n\n"+
 			"\tssh %s@%s\n\n"+
+			"\tPassword: %s%s\n"+
+			"\tConfirm Password: %s%s\n\n"+
 			"He also loves casting spells 🪄.\n\n"+
 			"%s",
 		utils.Title().Render("Configure network access to your Beepy"),
-		m.username.View(),
-		m.host.View(),
+		m.username.View(), m.host.View(),
+		m.password.View(), validation,
+		m.confirmation.View(), validation,
 		utils.Button(m.buttonFocused).Render("Next"),
 	)
 }
---
+++ ssh/validation.go
@@ -0,0 +1,25 @@
+package ssh
+
+import (
+	"github.com/figbert/beepy/utils"
+
+	"github.com/charmbracelet/bubbletea"
+	gloss "github.com/charmbracelet/lipgloss"
+)
+
+var (
+	okValidation = gloss.NewStyle().
+			Foreground(utils.Green).
+			SetString("✔")
+	errValidation = gloss.NewStyle().
+			Foreground(utils.Red).
+			SetString("✘")
+)
+
+type passwordValidationMsg bool
+
+func validatePassword(password, confirmation string) tea.Cmd {
+	return func() tea.Msg {
+		return passwordValidationMsg(len(password) > 0 && password == confirmation)
+	}
+}
